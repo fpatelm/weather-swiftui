2a270acc7916d00c2f9339963eb56512b1333c7e
Faizal Patel <faizalmw@hotmail.com>
2023-12-31 12:16:32 +0000

    first commit

---
+++ Waeather/WeatherButton.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButton.swift
+//  Waeather
+//
+//  Created by Faizal Patel on 31/12/2023.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButton()
+}
