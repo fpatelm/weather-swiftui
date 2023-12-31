6dab4e4493f472d26ad35ea08060ef0c97a0157c
Faizal Patel <faizalmw@hotmail.com>
2023-12-31 09:26:59 +0000

    Initial Commit

---
+++ Waeather/WaeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaeatherApp.swift
+//  Waeather
+//
+//  Created by Faizal Patel on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
